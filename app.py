85e015f98f90efb15049a65d9f843800208a83a0
Malav Jigneshbhai Gajera <malavgajera250@gmail.com>
2025-07-10 16:05:29 -0400

    Created minimal basic app for practice

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+
+    return render_template("index.html")
+    # return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def products():
+    return "<p>Products Page</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
